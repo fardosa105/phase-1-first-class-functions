cd68ff09ef8089ca8323b012889c9687a2e7ae7c
+ <fadosa.abdirhman@student.moringaschool>
2024-07-06 05:53:43 +0300

    modify

---
+++ index.js
@@ -0,0 +1,30 @@
+// index.js
+
+// Function that receives a function and calls it
+function receivesAFunction(callback) {
+    callback();
+}
+
+// Function that returns a named function
+function returnsANamedFunction() {
+    function namedFunction() {
+        console.log("This is a named function");
+    }
+    return namedFunction;
+}
+
+// Function that returns an anonymous function
+function returnsAnAnonymousFunction() {
+    return function() {
+        console.log("This is an anonymous function");
+    };
+}
+
+// Export the functions to be used in the tests
+module.exports = {
+    receivesAFunction,
+    returnsANamedFunction,
+    returnsAnAnonymousFunction
+};
+
+  
